0aff4a311c9c4278e01d349b0caa13af6748cb5e
Gopinath krishnamoorthy <gopinathkrishnamoorthy@Gopinaths-MacBook-Pro.local>
2018-12-28 15:32:02 +0530

    added mail event

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express"),
-    app = express();
+  app = express();
 
 var port = process.env.PORT || 8080;
+const nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'testerm818@gmail.com',
+    pass: '123@qwer'
+  }
+});
 app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
@@ -10,6 +18,60 @@ app.get("/sayHello", function (request, response) {
   response.end("Hello " + user_name + "!");
 });
 
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+app.get("/sendics", function (req, res) {
+  if (req.query.email) {
+    if (validateEmail(req.query.email)) {
+      var event = "BEGIN:VCALENDAR\n" +
+        "X-LOTUS-CHARSET:UTF-8\n" +
+        "VERSION:2.0\n" +
+        "BEGIN:VTIMEZONE\n" +
+        "TZID:Asia/Calcutta\n" +
+        "END:VTIMEZONE\n" +
+        "METHOD:REQUEST\n" +
+        "BEGIN:VEVENT\n" +
+        "UID:ics.terminsysteme.de1544262195\n" +
+        "DTSTAMP:20181208T104315\n" +
+        "CLASS:PUBLIC\n" +
+        "DTSTART;TZID=Asia/Calcutta:20190210T183000\n" +
+        "DTEND;TZID=Asia/Calcutta:20190210T223000\n" +
+        "LOCATION:R S Kalyana Mandapam,Oragadam Rd, Vijayalakshmi Puram, Ambattur, Chennai, Tamil Nadu 600053\n" +
+        "SUMMARY:Sasi weds Priya\n" +
+        "DESCRIPTION:wedding reception\n" +
+        "BEGIN:VALARM\n" +
+        "ACTION:DISPLAY\n" +
+        "TRIGGER:-PT2880M\n" +
+        "DESCRIPTION:Erinnerung\n" +
+        "END:VALARM\n" +
+        "END:VEVENT\n" +
+        "END:VCALENDAR\n";
+
+      const mailOptions = {
+        from: 'gopiforroses@gmail.com', // sender address
+        to: req.query.email, // list of receivers
+        subject: 'Sasi weds Priya,Save the date 10th Feb 2019', // Subject line
+        html: '<p>Hi,<br/>Thanks for time.We look forward for your blessings in person on the day of occassion.<br/><br/> With Love <br/>Sasi & Priya</p>', // plain text body
+        attachments: [{ // file on disk as an attachment
+          content: event,
+          filename: 'SasiwedsPriya.ics',
+          type: 'text/html',
+          disposition: 'attachment',
+          content_id: 'mytext'
+        }, ]
+      };
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err)
+          console.log(err)
+        else
+          console.log(info);
+      });
+    }
+  }
+});
 app.listen(port);
 console.log("Listening on port ", port);
 
